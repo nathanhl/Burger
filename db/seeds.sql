9becedf8ebad13ab02d014699d6f4971b0e17409
Nathan Leung <Nathan@Nathans-MacBook-Pro-2.local>
2017-05-13 09:27:39 -0500

    first commit

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+insert into burgers_tbl(burger_name) values ('Cheeseburger');
+insert into burgers_tbl(burger_name) values ('Hamburger');
+insert into burgers_tbl(burger_name) values ('Bacon Cheeseburger');
+insert into burgers_tbl(burger_name) values ('Turkey Burger');
+insert into burgers_tbl(burger_name) values ('Salmon Burger');
